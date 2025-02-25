c7f46c7018d117cb95c4d6027dc4d6660f727799
anibaltorresp <anibal.torresp@gmail.com>
2025-02-24 20:14:55 -0500

    initial project

---
+++ src/main/java/pe/edu/idat/app_transaccion_idat/AppTransaccionIdatApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_transaccion_idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTransaccionIdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTransaccionIdatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_transaccion_idat/AppTransaccionIdatApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_transaccion_idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTransaccionIdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
